56a8d6164dd39c2131f6ecd04ae158139f2fb04e
Aily-Hhh <78144867+aily-hhh@users.noreply.github.com>
2023-08-08 23:58:36 +0300

    adapter

---
+++ app/src/main/java/ru/mirea/cocktailbar/ui/cocktailList/CocktailsListAdapter.kt
@@ -0,0 +1,57 @@
+package ru.mirea.cocktailbar.ui.cocktailList
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import ru.mirea.cocktailbar.R
+import ru.mirea.cocktailbar.data.model.Cocktail
+
+class CocktailsListAdapter: RecyclerView.Adapter<CocktailsListAdapter.CocktailViewHolder>() {
+
+    inner class CocktailViewHolder(view: View): RecyclerView.ViewHolder(view){
+        val name = view.findViewById<TextView>(R.id.headerCocktailCard)
+    }
+
+    private val callback = object: DiffUtil.ItemCallback<Cocktail>() {
+        override fun areItemsTheSame(oldItem: Cocktail, newItem: Cocktail): Boolean {
+            return oldItem.name == newItem.name
+        }
+
+        override fun areContentsTheSame(oldItem: Cocktail, newItem: Cocktail): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    val differ = AsyncListDiffer(this, callback)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CocktailViewHolder {
+        return CocktailViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.cocktail_list_item, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: CocktailViewHolder, position: Int) {
+        val current = differ.currentList[position]
+        holder.name.text = current.name
+
+        holder.itemView.apply {
+            setOnClickListener{
+                onItemClickListener?.let { it(current) }
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    private var onItemClickListener: ((Cocktail) -> Unit)? = null
+
+    fun setOnItemClickListener(listener: (Cocktail) -> Unit) {
+        onItemClickListener = listener
+    }
+} 
