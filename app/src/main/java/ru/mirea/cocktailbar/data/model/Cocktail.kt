e6579e3c122126e6444f75594557433411326bb0
Aily-Hhh <78144867+aily-hhh@users.noreply.github.com>
2023-08-16 14:10:51 +0300

    DetailCocktailFragment

--- app/src/main/java/ru/mirea/cocktailbar/data/model/Cocktail.kt
+++ app/src/main/java/ru/mirea/cocktailbar/data/model/Cocktail.kt
@@ -1,5 +1,7 @@
 package ru.mirea.cocktailbar.data.model
 
+import android.os.Parcel
+import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
@@ -9,9 +11,42 @@ data class Cocktail(
     @PrimaryKey(autoGenerate = true)
     val id: Long = 0,
     @ColumnInfo(name = "name")
-    var name: String,
+    var name: String?,
     @ColumnInfo(name = "description")
-    var description: String,
+    var description: String?,
     @ColumnInfo(name = "recept")
-    var recept: String
-)
+    var recept: String?,
+    @ColumnInfo(name = "image")
+    var image: String?
+): Parcelable {
+    constructor(parcel: Parcel) : this(
+        id = parcel.readLong(),
+        name = parcel.readString(),
+        description = parcel.readString(),
+        recept = parcel.readString(),
+        image = parcel.readString()
+    )
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.writeLong(id)
+        dest.writeString(name)
+        dest.writeString(description)
+        dest.writeString(recept)
+        dest.writeString(image)
+    }
+
+    companion object CREATOR : Parcelable.Creator<Cocktail> {
+        override fun createFromParcel(parcel: Parcel): Cocktail {
+            return Cocktail(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Cocktail?> {
+            return arrayOfNulls(size)
+        }
+    }
+
+}
--- app/src/main/java/ru/mirea/cocktailbar/ui/detailCocktail/DetailCocktailFragment.kt
+++ app/src/main/java/ru/mirea/cocktailbar/ui/detailCocktail/DetailCocktailFragment.kt
@@ -5,18 +5,77 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.net.toUri
+import androidx.core.os.bundleOf
+import androidx.fragment.app.viewModels
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.navArgs
 import dagger.hilt.android.AndroidEntryPoint
 import ru.mirea.cocktailbar.R
+import ru.mirea.cocktailbar.data.model.Cocktail
+import ru.mirea.cocktailbar.data.model.Ingredient
+import ru.mirea.cocktailbar.data.viewModel.CocktailViewModel
+import ru.mirea.cocktailbar.data.viewModel.IngredientViewModel
+import ru.mirea.cocktailbar.databinding.FragmentDetailCocktailBinding
 
 
 @AndroidEntryPoint
 class DetailCocktailFragment : Fragment() {
 
+    private var _binding: FragmentDetailCocktailBinding? = null
+    private val binding get() = _binding!!
+    private val viewModelCocktail by viewModels<CocktailViewModel>()
+    private val viewModelIngredient by viewModels<IngredientViewModel>()
+    private val bundleArgs by navArgs<DetailCocktailFragmentArgs>()
+    private var currentCocktail: Cocktail? = null
+    private var listIngredient: List<Ingredient> = listOf()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail_cocktail, container, false)
+        _binding = FragmentDetailCocktailBinding.inflate(inflater, container, false)
+        currentCocktail = bundleArgs.currentCocktail
+        listIngredient = viewModelIngredient.getIngredients(currentCocktail!!.id).value ?: listOf()
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.nameCocktailDetail.text = currentCocktail?.name
+        binding.descriptionCocktailDetail.text = currentCocktail?.description
+        binding.txtRecipeDetails.text = currentCocktail?.recept
+
+        if (listIngredient.isNotEmpty()) {
+            for (i in listIngredient) {
+                var strIngredients: String = ""
+                if (i.id == listIngredient.last().id) {
+                    strIngredients.plus(i.name)
+                } else {
+                    strIngredients.plus(i.name + "\n•.•.•\n")
+                }
+            }
+        }
+
+        if (currentCocktail?.image != null) {
+            binding.imgHeaderCocktailDetail.setImageURI(currentCocktail?.image!!.toUri())
+        }
+
+        binding.btnEditCocktail.setOnClickListener {
+            val bundle = bundleOf("current_cocktail" to currentCocktail)
+            view.findNavController().navigate(
+                R.id.action_detailCocktailFragment_to_addCocktailFragment,
+                bundle
+            )
+        }
+
+        binding.btnDeleteCocktail.setOnClickListener {
+            // TODO: dialog
+            viewModelCocktail.delete(cocktail = currentCocktail!!)
+            Toast.makeText(requireActivity(), "Deleted", Toast.LENGTH_SHORT).show()
+            view.findNavController().popBackStack()
+        }
     }
 }
